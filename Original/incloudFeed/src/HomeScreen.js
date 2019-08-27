4027536226806ce0a0213065895be8257e0b86e1
Incloud <incloud@Inclouds-MacBook-Pro.local>
2019-08-27 14:59:29 +0700

    dgdsg

--- Original/incloudFeed/src/HomeScreen.js
+++ Original/incloudFeed/src/HomeScreen.js
@@ -1,38 +1,59 @@
-import React, { Component } from 'react'
-import { Text, View, ImageBackground, TextInput } from 'react-native'
-import Icon from "react-native-vector-icons/FontAwesome";
-import AsyncStorage from "@react-native-community/async-storage";
+import React, {Component} from 'react';
+import {
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import AsyncStorage from '@react-native-community/async-storage';
 
 class TextEntry extends Component {
   render() {
     return (
-      <View style={{ flexDirection: "row", alignItems: "flex-end" }}>
-            <Icon
-              name={this.props.iconName}
-              size={40}
-              style={{ width: 40 }}
-              color="#0008"
-            />
-            <TextInput
-              autoCapitalize="none"
-              secureTextEntry={this.props.isSecure}
-              style={{
-                flex: 1,
-                borderColor: "#0005",
-                borderWidth: 1,
-                marginLeft: 16,
-                height: 40,
-                borderRadius: 5,
-                paddingLeft: 8
-              }}
-              placeholder={this.props.defaultHolder}
-            />
-          </View>
+      <View style={{flexDirection: 'row', alignItems: 'flex-end'}}>
+        <Icon
+          name={this.props.iconName}
+          size={40}
+          style={{width: 40}}
+          color="#0008"
+        />
+        <TextInput
+          autoCapitalize="none"
+          onChangeText={this.props.onChangeValue}
+          secureTextEntry={this.props.isSecure}
+          style={{
+            flex: 1,
+            borderColor: '#0005',
+            borderWidth: 1,
+            marginLeft: 16,
+            height: 40,
+            borderRadius: 5,
+            paddingLeft: 8,
+          }}
+          placeholder={this.props.defaultHolder}
+        />
+      </View>
     );
   }
 }
 
 export default class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
+  onClickLoginBtn = () => {
+    alert(this.state.username);
+    alert(this.state.password);
+  };
+
   render() {
     return (
       <ImageBackground
@@ -40,17 +61,43 @@ export default class HomeScreen extends Component {
         source={require('./assets/img/bg.png')}>
         <View
           style={{
-            backgroundColor: "#FFF7",
+            backgroundColor: '#FFF7',
             margin: 16,
             padding: 16,
-            borderRadius: 5
-          }}
-        >
+            borderRadius: 5,
+          }}>
           {/* UserName section */}
-          <TextEntry iconName="user" isSecure={false} defaultHolder="Username"/>
-          <TextEntry iconName="lock" isSecure={true} defaultHolder="Password"/>
+          <TextEntry
+            iconName="user"
+            isSecure={false}
+            defaultHolder="Username"
+            onChangeValue={text => {
+              this.setState({username: text});
+            }}
+          />
+          <TextEntry
+            iconName="lock"
+            isSecure={true}
+            defaultHolder="Password"
+            onChangeValue={text => {
+              this.setState({password: text});
+            }}
+          />
+
+          <TouchableOpacity
+            onPress={this.onClickLoginBtn}
+            style={{
+              backgroundColor: '#0096FB',
+              height: 35,
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: 3,
+              marginTop: 16,
+            }}>
+            <Text style={{color: 'white', fontWeight: 'bold'}}>Login</Text>
+          </TouchableOpacity>
         </View>
       </ImageBackground>
-    )
+    );
   }
 }
