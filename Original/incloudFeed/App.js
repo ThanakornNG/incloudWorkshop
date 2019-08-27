fa4469b52f898ac85de66ab0d91ef2592bc54baf
Incloud <incloud@Inclouds-MacBook-Pro.local>
2019-08-27 11:43:21 +0700

    asdf

--- Original/incloudFeed/App.js
+++ Original/incloudFeed/App.js
@@ -4,16 +4,7 @@ import {Text, View} from 'react-native';
 export default class App extends Component {
   render() {
     return (
-      <View style={{flex:1, flexDirection:"column"}}>
-        <View style={{flex: 1, backgroundColor: 'red'}}>
-          <Text> textInComponent </Text>
-        </View>
-        <View style={{flex: 1, backgroundColor: 'blue'}}>
-          <Text> text2 </Text>
-        </View>
-        <View style={{flex: 1, backgroundColor: 'green'}}>
-          <Text> text3 </Text>
-        </View>
+      <View style={{flex:1, flexDirection:"row", alignItems:"center"}}>
       </View>
     );
   }
