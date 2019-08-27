9366e9655bf1b0d0de002b64dd54bad9e930526b
Incloud <incloud@Inclouds-MacBook-Pro.local>
2019-08-27 11:26:27 +0700

    1111

--- Original/incloudFeed/App.js
+++ Original/incloudFeed/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style={{flex:1, flexDirection:"column"}}>
+        <View style={{flex: 1, backgroundColor: 'red'}}>
+          <Text> textInComponent </Text>
+        </View>
+        <View style={{flex: 1, backgroundColor: 'blue'}}>
+          <Text> text2 </Text>
+        </View>
+        <View style={{flex: 1, backgroundColor: 'green'}}>
+          <Text> text3 </Text>
+        </View>
       </View>
-    )
+    );
   }
 }
